f01cfefd9f32d0a260f9404b56ed7e7c5a91e8a5
Jkon1513 <jkon1513@gmail.com>
2019-10-09 21:50:23 -0700

    created basic user form component

--- frontend/components/UserForm.js
+++ frontend/components/UserForm.js
@@ -1,32 +1,68 @@
 import React,{useState, useEffect} from "react";
+import {withStyles} from "@material-ui/styles";
+import TextField from "@material-ui/core/TextField";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Button from "@material-ui/core/Button";
 import useControlledInput from "./hooks/useControlledForm";
 
-function UserForm() {
-    const [userName, setUserName, resetUserName] = useControlledInput("");
+const styles={
+    UserForm: {
+        width:"100%",
+        display: "flex",
+        flexDirection: "column",
+        JustifyContent: "center",
+        alignItems: "center"
+    }
+}
+
+function UserForm(props) {
+    const [email, setEmail, resetEmail] = useControlledInput("");
     const [passWord, setPassWord, resetPassWord] = useControlledInput("");
 
-    const handleChange = (evt) => setUserName(evt);
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        alert(`username: ${userName}, password: ${passWord}`);
-        resetUserName();
+        //put signing/login here
+        alert(`username: ${email}, password: ${passWord}`);
+        resetEmail();
         resetPassWord();
     }
-    useEffect(() => {
-        console.log("rendering");
-    })
+    const {UserForm} = props.classes;
+    const {formName} = props;
     return(
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">
-                <span>username:</span>
-                <input type="text" id="username" value={userName} onChange={setUserName}/>
-            </label>
-            <label htmlFor="password">
-                <span>password:</span>
-                <input type="text" id="password" value={passWord} onChange={setPassWord}/>
-            </label>
-            <input type="submit"/>
+        <form onSubmit={handleSubmit} className={UserForm}>
+            <h1>{formName}</h1>
+            <TextField
+                id="email"
+                label="email:"
+                type="email"
+                onChange={setEmail}
+                style={{ margin: 8 }}
+                placeholder="Enter your email"
+                fullWidth
+                margin="normal"
+                variant="filled"
+                InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <AccountCircle />
+                      </InputAdornment>
+                    ),
+                  }}
+            />
+            <TextField
+                id="password"
+                type="password"
+                onChange={setPassWord}
+                style={{ margin: 8 }}
+                placeholder="Enter your password"
+                fullWidth
+                margin="normal"
+                variant="filled"
+            />
+            <Button variant="contained" type="submit">Submit</Button>
         </form>
+
     )
 }
-export default UserForm;
+export default withStyles(styles)(UserForm);
